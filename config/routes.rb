9e6d63947923296d818fbd162509c02a117956a5
Tony Huynh <huynh.tony55@gmail.com>
2019-07-16 14:04:49 +1000

    dropped travellers table & added columns to user table

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'main', to: 'main#index', as: 'main_index'
   resources :guides
-  resources :travellers
   resources :bookings
   get 'guides/:guide_id/bookings/new', to: 'bookings#new', as: 'new_guide_booking'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

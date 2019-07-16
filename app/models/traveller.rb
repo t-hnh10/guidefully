a7be8d5a48555b01388d538e540b83c5189a7277
natkat9 <42442614+natkat9@users.noreply.github.com>
2019-07-16 15:20:25 +1000

    removed traveller controller & model

--- app/models/traveller.rb
+++
@@ -1,9 +0,0 @@
-class Traveller < ApplicationRecord
-    has_many :bookings
-
-    def self.create_traveller(name, phone_number)
-        traveller = Traveller.new(name: name, phone_number: phone_number)
-        traveller.save
-        return traveller
-    end
-end
--- test/models/traveller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TravellerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end

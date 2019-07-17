1cb7090986c403fb57b8a9f68493c15e3240e1f1
Tony Huynh <huynh.tony55@gmail.com>
2019-07-17 11:53:30 +1000

    added countries gem & devise now conforms with bulma

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,9 @@
 module MainHelper
+    def emoji_flag_for_country(country)
+        ISO3166::Country.find_country_by_name(country).emoji_flag
+    end
+
+    def country_code(country)
+        ISO3166::Country.find_country_by_name(country).country_code
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 Guide.destroy_all
 
-Guide.create(name: 'Lee', phone_number: '0404 040 404', city: 'Quantum City', country: 'Lithuania', cost: 12)
-Guide.create(name: 'Dmitry', phone_number: '0404 040 404', city: 'Kazan', country: 'Russia', cost: 25)
-Guide.create(name: 'Chulsoo', phone_number: '0404 040 404', city: 'Hangang', country: 'South Korea', cost: 15)
-Guide.create(name: 'Phoebe', phone_number: '0404 040 404', city: 'New York City', country: 'United States of America', cost: 20)
+Guide.create(name: 'Jokubas', phone_number: '8422 84920', city: 'Šiauliai', country: 'Lithuania', cost: 12)
+Guide.create(name: 'Dmitry', phone_number: '8 499 829-22-80', city: 'Kazan', country: 'Russian Federation', cost: 25)
+Guide.create(name: 'Chulsoo', phone_number: '02-312-3456', city: 'Hanyang', country: 'Korea (Republic of)', cost: 15)
+Guide.create(name: 'Phoebe', phone_number: '202-555-0168', city: 'Portland', country: 'United States of America', cost: 20)
